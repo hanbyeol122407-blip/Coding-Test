ee673ffd447344d17f5d068c5e72f3512c8e74df
one_star <one_star@one-starui-MacBookPro.local>
2026-02-25 16:21:38 +0900

    solve: 출력 문제 풀이

--- Programmers/260225_Pro_출력.py
+++ Programmers/260225_Pro_출력.py
@@ -1,7 +1,7 @@
 string_msg = "Spring is beginning"
-int_val = 13
-string_val = "310"
+int_val = 3
+string_val = "3"
 
 print(string_msg)
-print(int_val)
-print(string_val)
+print(int_val + 10)
+print(string_val + "10")
